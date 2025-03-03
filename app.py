edb878818ea224a0b4e947b9235ba806f5556afe
root <root@ip-172-31-40-113.ap-south-1.compute.internal>
2025-03-03 10:47:53 +0000

    web application is running

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello, Docker!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Docker!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
+
